1af09074c45187cc9f7d21727e3f84b517706207
Kirill Ivashkin <kirillkus@yandex.ru>
2021-09-16 05:14:26 +0300

    Cath error change

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -27,7 +27,7 @@ export const ReviewForm = ({ productId, isOpened, className, ...props }: ReviewF
 				setError('Что-то пошло не так');
 			}
 		} catch (e) {
-			setError(e.message);
+			setError('Что-то пошло не так');
 		}
 	};
 
